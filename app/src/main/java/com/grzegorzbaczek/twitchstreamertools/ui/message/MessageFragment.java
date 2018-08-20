14257614ff6023a33986bd1dc87a2779d7933917
grzegorz.baczek <grzegorz.baczek@intive.com>
2018-08-20 11:11:25 +0200

    Fixed issue with bottomnavigationview selected item highlight, added icons

--- app/src/main/java/com/grzegorzbaczek/twitchstreamertools/ui/message/MessageFragment.java
+++ app/src/main/java/com/grzegorzbaczek/twitchstreamertools/ui/message/MessageFragment.java
@@ -1,30 +1,50 @@
 package com.grzegorzbaczek.twitchstreamertools.ui.message;
 
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.grzegorzbaczek.twitchstreamertools.R;
+import com.grzegorzbaczek.twitchstreamertools.databinding.FragmentMessageBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class MessageFragment extends Fragment {
 
-
-    public MessageFragment() {
-        // Required empty public constructor
-    }
-
+    private MessageViewModel viewModel;
+    private FragmentMessageBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        setupBinding(inflater,container);
+
+        setHasOptionsMenu(true);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.message_fragment_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return false;
+    }
+
+    private void setupBinding(LayoutInflater inflater, ViewGroup container) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_message, container, false);
+        viewModel = ViewModelProviders.of(this).get(MessageViewModel.class);
+        binding.setViewModel(viewModel);
     }
 
 }
---
+++ app/src/main/java/com/grzegorzbaczek/twitchstreamertools/ui/message/MessageViewModel.java
@@ -0,0 +1,7 @@
+package com.grzegorzbaczek.twitchstreamertools.ui.message;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MessageViewModel extends ViewModel {
+
+}
